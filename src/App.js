123b8d9402732895dac9bcfed46f8da0a5ba60a7
Benard <benardcharumbira@gmail.com>
2020-08-29 23:48:33 +0200

    added products component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Home from "../src/components/Home/Home";
+import Login from "../src/components/Login/Login";
+import Checkout from "../src/components/Checkout/Checkout";
+
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route exact path="/checkout" render={() => <h1>Checkout</h1>} />
-          <Route exact path="/login" render={() => <h1>Login</h1>} />
-          <Route exact path="/">
-            <Header />
-            <h1>Home page</h1>
-          </Route>
+          <Route exact path="/checkout" component={Checkout} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </div>
     </Router>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Header from "../Header/Header";
+import Product from "../Product/Product";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Header />
+      <img
+        className="home__image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt=""
+      />
+
+      {/* Product id, title, price, rating, image */}
+      <div className="home__row">
+        <Product
+          id="1"
+          title="Midnight sun"
+          price={29.99}
+          rating={3}
+          image="https://images-na.ssl-images-amazon.com/images/I/81j8pE4PWXL.__BG0,0,0,0_FMpng_AC_SY220_.jpg"
+        />
+        <Product
+          id="2"
+          title="Amazon Basics Nylon Braided Lightning to USB A Cable, MFi Certified Apple iPhone Charger, Dark Gray, 6-Foot - Pack of 10"
+          price={75.99}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/71p11135VSL._AC_UL480_FMwebp_QL65_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="3"
+          title="Amazon Basics Air-Circulator 3 Speed Small Room Floor Fan"
+          price={19.99}
+          rating={3}
+          image="https://m.media-amazon.com/images/I/91pOLl2YUsL._AC_UL480_FMwebp_QL65_.jpg"
+        />
+        <Product
+          id="4"
+          title="Dual Arm Monitor Stand - Lift Engine Arm Mount, Aluminum - Silver"
+          price={169.99}
+          rating={5}
+          image="https://m.media-amazon.com/images/I/71PyeB2baxL._AC_UL480_FMwebp_QL65_.jpg"
+        />
+        <Product
+          id="5"
+          title="Racing Style Office Chair with Removable Headrest and High Back Cushion - Red"
+          price={99.99}
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61JUe6eJUZL._AC_UL480_FMwebp_QL65_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="6"
+          title="Garden Tool Collection - 3-Way Front Push Button Nozzle with Zinc Alloy Base"
+          price={49.99}
+          rating={2}
+          image="https://m.media-amazon.com/images/I/71e8hwb2ZsL._AC_UL480_FMwebp_QL65_.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
