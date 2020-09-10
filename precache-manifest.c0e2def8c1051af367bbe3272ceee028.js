002da46693c2dbccdcb2eec83b116ae822179081
Benard <benardcharumbira@gmail.com>
2020-09-10 17:43:47 +0200

    Updates

---
+++ precache-manifest.c0e2def8c1051af367bbe3272ceee028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cb4866b98bf57c9256798c067167d30",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "eef925afc51c281a7a48",
+    "url": "/amazon-clone/static/css/main.bd99ff9c.chunk.css"
+  },
+  {
+    "revision": "62807bbb19627536a00d",
+    "url": "/amazon-clone/static/js/2.3dcb8582.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/amazon-clone/static/js/2.3dcb8582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef925afc51c281a7a48",
+    "url": "/amazon-clone/static/js/main.bb819dbc.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  }
+]);
